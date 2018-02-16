da3b59450e722881c42851736b9b6923b1bef6c3
Emmanuel Rosario <rosario.emmanuel.bmf@gmail.com>
2018-02-16 03:11:47 -0500

    more classes

--- ussc/app/src/main/java/com/example/u/ussc/MarketplaceActivity.java
+++ ussc/app/src/main/java/com/example/u/ussc/MarketplaceActivity.java
@@ -3,13 +3,11 @@ package com.example.u.ussc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MarketplaceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-
-
+        setContentView(R.layout.activity_marketplace);
     }
 }
--- ussc/app/src/main/java/com/example/u/ussc/RegistrationActivity.java
+++ ussc/app/src/main/java/com/example/u/ussc/RegistrationActivity.java
@@ -1,13 +1,30 @@
 package com.example.u.ussc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        signup = (Button) findViewById(R.id.signupButton);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                main();
+            }
+        });
+    }
+
+    //test
+    private void main() {
+        Intent intent = new Intent(RegistrationActivity.this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
