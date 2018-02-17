fc350816ce10c06c1d0d3cfb6263f858ce9aab66
blundy <brett_lundy@student.uml.edu>
2018-02-17 13:24:13 -0500

    log in/ sign up first commit

--- ussc/app/src/main/java/com/example/u/ussc/MainMenuActivity.java
+++ ussc/app/src/main/java/com/example/u/ussc/MainMenuActivity.java
@@ -1,15 +1,52 @@
 package com.example.u.ussc;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.List;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
+    private DatabaseReference databaseReference;
+    private ListView listView;
+    private SearchView searchView;
+    private Button all;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        // Initialize Firebase Auth and Database Reference
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        final FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        if (mFirebaseUser == null || !(user.isEmailVerified())) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        }
+    }
+
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
--- ussc/app/src/main/java/com/example/u/ussc/RegistrationActivity.java
+++ ussc/app/src/main/java/com/example/u/ussc/RegistrationActivity.java
@@ -1,30 +1,127 @@
 package com.example.u.ussc;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationActivity extends AppCompatActivity {
-private Button signup;
+    protected EditText passwordEditText;
+    protected EditText emailEditText;
+    protected Button signUpButton;
+    private FirebaseAuth mFirebaseAuth;
+    private static final String TAG = "SignUp";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        signup = (Button) findViewById(R.id.signupButton);
-        signup.setOnClickListener(new View.OnClickListener() {
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        passwordEditText = (EditText)findViewById(R.id.passwordField);
+        emailEditText = (EditText)findViewById(R.id.emailField);
+        signUpButton = (Button)findViewById(R.id.signupButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                main();
+            public void onClick(View v) {
+                String password = passwordEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+
+                password = password.trim();
+                email = email.trim();
+
+                if (!isValidEmail(emailEditText.getText().toString())) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+                    builder.setMessage(R.string.signup_invaild_email_message)
+                            .setTitle(R.string.signup_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }else if (password.isEmpty() || email.isEmpty() ) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+                    builder.setMessage(R.string.signup_error_message)
+                            .setTitle(R.string.signup_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        sendEmailVerification();
+                                        Intent intent = new Intent(RegistrationActivity.this, LogInActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    } else {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(RegistrationActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle(R.string.login_error_title)
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
 
-    //test
-    private void main() {
-        Intent intent = new Intent(RegistrationActivity.this, MainMenuActivity.class);
-        startActivity(intent);
+    Boolean isValidEmail(String testEmail) {
+        String emailREGEX = "[A-Z0-9a-z._%+-]{1,61}+@[student]+\\.[uml]+\\.[edu]*$";
+        String emailTest = testEmail;
+        Pattern p = Pattern.compile(emailREGEX);
+        Matcher m = p.matcher(emailTest);   // get a matcher object
+        Boolean result = emailTest.matches(emailREGEX);
+        return result;
+
     }
+
+    private void sendEmailVerification() {
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(RegistrationActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
+    }
+
 }
+
+
