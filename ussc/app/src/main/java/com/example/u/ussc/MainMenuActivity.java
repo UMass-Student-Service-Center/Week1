822471d935cac336fe311ae09ea39df32c33b131
Emmanuel Rosario <rosario.emmanuel.bmf@gmail.com>
2018-02-21 12:51:58 -0500

    main menu (button)

--- ussc/app/src/main/java/com/example/u/ussc/MainMenuActivity.java
+++ ussc/app/src/main/java/com/example/u/ussc/MainMenuActivity.java
@@ -4,9 +4,12 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,7 +26,9 @@ public class MainMenuActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private ListView listView;
     private SearchView searchView;
-    private Button all;
+    private Button marketPlace;
+    private Button lost_and_found;
+    private Button advising;
     private ProgressDialog progressDialog;
 
     @Override
@@ -40,13 +45,61 @@ public class MainMenuActivity extends AppCompatActivity {
         if (mFirebaseUser == null || !(user.isEmailVerified())) {
             // Not logged in, launch the Log In activity
             loadLogInView();
-        }
-    }
+        }else{
 
+            //Buttons from the main menu
+            lost_and_found = (Button) findViewById(R.id.lostandfound);
+            marketPlace = (Button) findViewById(R.id.Marketplace);
+            advising = (Button) findViewById(R.id.advising);
+
+            //set up on click listener for all the buttons
+            //on click calls each activity
+            lost_and_found.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    goto_lost_and_found();
+                }
+            });
+
+            marketPlace.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    goto_marketplace_();
+                }
+            });
+
+            advising.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    goto_advising();
+                }
+            });
+
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
     private void loadLogInView() {
         Intent intent = new Intent(this, LogInActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void goto_lost_and_found() {
+        Intent intent = new Intent(MainMenuActivity.this, LostandFActivity.class);
+        startActivity(intent);
+    }
+    private void goto_marketplace_() {
+        Intent intent = new Intent(MainMenuActivity.this, MarketplaceActivity.class);
+        startActivity(intent);
+    }
+    private void goto_advising() {
+        Intent intent = new Intent(MainMenuActivity.this, AdvisingActivity.class);
         startActivity(intent);
     }
 }
