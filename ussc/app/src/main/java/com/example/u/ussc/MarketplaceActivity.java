bf821ef43f1d1fa04645312ee39ce2b9d958bb89
emmanrosa <emmanuel_rosario@student.uml.edu>
2018-04-14 12:47:44 -0400

    marketplace

--- ussc/app/src/main/java/com/example/u/ussc/MarketplaceActivity.java
+++ ussc/app/src/main/java/com/example/u/ussc/MarketplaceActivity.java
@@ -1,18 +1,159 @@
 package com.example.u.ussc;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MarketplaceActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
+    private DatabaseReference databaseReference;
+    private ListView listView;
+    private List<item_names> list_item_s;
+    private item_list_Adapter adapter;
+    private SearchView searchView;
+    private Button all;
+    private ProgressDialog progressDialog;
     private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marketplace);
+        // Initialize Firebase Auth and Database Reference
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        final FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        if (mFirebaseUser == null || !(user.isEmailVerified())) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        } else {
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            mFirebaseUser = mFirebaseAuth.getCurrentUser();
+            mUserId = mFirebaseUser.getUid();
+
+            list_item_s = new ArrayList<>();
+            listView = (ListView) findViewById(R.id.list_m);
+            searchView = (SearchView) findViewById(R.id.searchbook_m);
+            all = (Button) findViewById(R.id.all_m);
+
+            list_item_s = new ArrayList<>();
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Please wait loading.....");
+            progressDialog.show();
+
+            databaseReference = FirebaseDatabase.getInstance().getReference(add_item_marketplaceActivity.fb_database);
+
+            progressDialog.dismiss();
+
+            all.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            progressDialog.dismiss();
+                            list_item_s.clear();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                item_names ln = snapshot.getValue(item_names.class);
+                                //if (ln.getMUserId().equals(mUserId))
+                                list_item_s.add(ln);
+                            }
+
+                            adapter = new item_list_Adapter(MarketplaceActivity.this, R.layout.lostandf_item, list_item_s);
+                            listView.setAdapter(adapter);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            progressDialog.dismiss();
+                        }
+                    });
+                }
+            });
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+                @Override
+                public boolean onQueryTextSubmit(String query) {return false; }
+
+                @Override
+                public boolean onQueryTextChange(final String newText) {
+                    //Query query = databaseReference.orderByChild("isbn").startAt(newText).endAt("~");
+                    Query query = databaseReference.orderByChild("title").startAt(newText).endAt(newText);
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            progressDialog.dismiss();
+                            list_item_s.clear();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                                item_names ln = snapshot.getValue(item_names.class);
+                                list_item_s.add(ln);
+                            }
+                            adapter = new item_list_Adapter(MarketplaceActivity.this,R.layout.lostandf_item, list_item_s);
+                            listView.setAdapter(adapter);
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            progressDialog.dismiss();
+                        }
+                    });
+                    return false;
+                }
+            });
+
+        }
+
+    }
+
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                //
+                //account();
+                // Exit activity
+                // finish();
+                return true;
+            //break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
     private void goto_lost_and_found() {
         Intent intent = new Intent(MarketplaceActivity.this, LostandFActivity.class);
